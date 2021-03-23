5232004b5043a64263bf4f1d376ffdb693f7dfbb
silil <liliana.miilan@gmail.com>
2021-03-23 17:41:39 -0600

    S3Client

--- scripts/luigi/ex3.py
+++ scripts/luigi/ex3.py
@@ -6,6 +6,9 @@ import luigi
 import luigi.contrib.s3
 import boto3
 import os
+import yaml
+
+from luigi.contrib.s3 import S3Client
 
 
 class S3Task(luigi.Task):
@@ -16,18 +19,25 @@ class S3Task(luigi.Task):
     etl_path = luigi.Parameter()
     year = luigi.Parameter()
     month = luigi.Parameter()
-
+ 
     def run(self):
-        ses = boto3.session.Session(profile_name='mge', region_name='us-west-2')
-        s3_resource = ses.resource('s3')
-
-        print(ses)
+        #ses = boto3.session.Session(profile_name='mge', region_name='us-west-2')
+        #s3_resource = ses.resource('s3')
+        #print(ses)
 
         with self.output().open('w') as output_file:
             output_file.write("test,luigi,s3")
 
 
     def output(self):
+        with open ("credentials.yaml", 'r') as f:
+            config = yaml.safe_load(f)
+
+        s3_creds = config['s3']
+
+        client_s3 = S3Client(aws_access_key_id=s3_creds['aws_access_key_id'],
+                             aws_secret_access_key=s3_creds['aws_secret_access_key'])
+
         output_path = "s3://{}/{}/{}/{}/YEAR={}/MONTH={}/test.csv".\
         format(self.bucket,
         self.root_path,
@@ -36,4 +46,4 @@ class S3Task(luigi.Task):
         self.year,
         str(self.month))
 
-        return luigi.contrib.s3.S3Target(path=output_path)
+        return luigi.contrib.s3.S3Target(path=output_path, client=client_s3)
